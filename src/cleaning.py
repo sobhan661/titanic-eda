de15b0cdd23401b434c0fb4a153975f506205a2f
sobhan661 <sobhantheimpaler@gmail.com>
2026-06-28 16:44:48 +0330

    Create clean_titanic function using 02 notebooks codes

---
+++ src/cleaning.py
@@ -0,0 +1,19 @@
+from data.raw.titanic import df
+
+
+def clean_titanic(df):
+    # The original data set won't get mutated
+    df = df.copy()
+
+    # Drop deck column
+    df = df.drop(columns=["deck"])
+
+    # Fill age column missing values with the age median
+    df["age"] = df["age"].fillna(value=df["age"].median())
+
+    # Fill embarked and embark_town missing values with each column first mode
+    df["embarked"] = df["embarked"].fillna(value=df["embarked"].mode()[0])
+    df["embark_town"] = df["embark_town"].fillna(value=df["embark_town"].mode()[0])
+
+    # Returns the edited df
+    return df
